3656237345bf97596bbfba46057e42aa78517a4a
Amanda <amandalovescoding@gmail.com>
2016-09-09 17:39:00 +0800

    my first commit

---
+++ HTML5/draw.js
@@ -0,0 +1,12 @@
+console.log("Hello Iam working.")
+
+var c =document.getElementById("mycanvas");
+var context=c.getContext("2d");
+
+
+
+// context.fillStyle="rgb(255,0,0)";
+// context.fillRect(100,100,100,100);
+// context.strokeStyle = "rgb(0,0,255)"
+// context.strokeRect(80,80,140,140);
+// context.clearRect(120,120,60,60);

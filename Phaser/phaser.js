cff9fb3070cc14d025b1c5cab74535e521bdc789
Amanda <amandalovescoding@gmail.com>
2016-12-09 17:21:51 +0800

    added assets

--- Phaser/phaser.js
+++ Phaser/phaser.js
@@ -73,7 +73,7 @@ function create(){
 		// animate the sprite
 		enemy3.animations.add('left', [0,1], 10, true);
 		enemy3.animations.add('right', [2,3], 10, true);
-		game.physics.arcade.enable(enemy 3);
+		game.physics.arcade.enable(enemy3);
 		// creating physics for enemy sprite
 		enemy3.body.bounce.y = 0.2;
 		enemy3.body.gravity.y = 500;
